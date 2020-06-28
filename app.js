f7155818e17f06de65ed434b08341a8987e06352
rakshit1994 <singhrakshit@gmail.com>
2020-06-28 20:50:14 +0530

    Openshift Sample Application

---
+++ app.js
@@ -0,0 +1,33 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+require('dotenv').config()
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+
+const port = process.env.PORT || 3000
+
+console.log('The value of PORT is:', process.env.PORT);
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+
+module.exports = app;
